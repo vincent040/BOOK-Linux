1a9fc83344e8d5df66d4af4b7451cc828c666f1d
vincent040 <18620373040@163.com>
2018-02-28 22:44:39 +0800

    删除了数据结构代码，增加设备操作代码

---
+++ chapter03/3.1 触摸屏/ts_raw.c
@@ -0,0 +1,73 @@
+//////////////////////////////////////////////////////////////////
+//
+//  Copyright(C), 2013-2016, GEC Tech. Co., Ltd.
+//
+//  File name: GPLE/ch03/3.1/ts_raw.c
+//
+//  Author: Vincent Lin (林世霖)  微信公众号：秘籍酷
+//
+//  Date: 2018-3
+//  
+//  Description: 演示了使用函数read()直接读取触摸屏数据的例子
+//
+//  GitHub: github.com/vincent040   Bug Report: 2437231462@qq.com
+//
+//////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <errno.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <linux/input.h>
+
+int main(int argc, char **argv)
+{
+	// 打开触摸屏设备
+	int ts = open("/dev/input/event0", O_RDONLY);
+
+	// 定义输入信息结构体
+	struct input_event buf;
+	bzero(&buf, sizeof(buf));
+
+	while(1)
+	{
+		// 读取触摸屏数据
+		read(ts, &buf, sizeof(buf));
+
+		// 1，读取到触摸屏被按下或被松开事件
+		if(buf.type == EV_KEY && buf.code == BTN_TOUCH)
+		{
+			if(buf.value > 0) // 按下了触摸屏
+			{
+				printf("开始读取手指坐标...\n");
+			}
+
+			if(buf.value == 0) // 松开了触摸屏
+			{
+				printf("结束.\n");
+				break;
+			}
+		}
+
+		// 2，读取到触摸屏X轴坐标值事件
+		if(buf.type == EV_ABS && buf.code == ABS_X)
+		{
+			printf("x: %d\n", buf.value);
+		}
+
+		// 3，读取到触摸屏Y轴坐标值事件
+		if(buf.type == EV_ABS && buf.code == ABS_Y)
+		{
+			printf("y: %d\n", buf.value);
+		}
+	}
+
+	return 0;
+}
