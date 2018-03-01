11d0251659bfc434c4530ea23af9b5f581e5fd42
vincent040 <18620373040@163.com>
2018-03-01 15:14:05 +0800

    修改程序版权信息

--- chapter02 Linux文件IO/2.2 文件操作/fopen_fclose.c
+++ chapter02 Linux文件IO/2.2 文件操作/fopen_fclose.c
@@ -1,18 +1,16 @@
-//////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////
 //
-//  Copyright(C), 2013-2016, GEC Tech. Co., Ltd.
+//  Copyright(C), 广州粤嵌通信科技股份有限公司
 //
-//  File name: GPLE/ch04/4.2/fopen_fclose.c
+//  作者: Vincent Lin (林世霖)  微信公众号：秘籍酷
 //
-//  Author: Vincent Lin (林世霖)  微信公众号：秘籍酷
+//  技术微店: http://weidian.com/?userid=260920190
+//  技术交流: 260492823（QQ群）
 //
-//  Date: 2016-3
-//  
-//  Description: 演示了fopen()和fclose()的基本用法
+//  文件: GPLE2.0/ch02/2.2/fopen_fclose.c
+//  描述: 演示了fopen()和fclose()的基本用法
 //
-//  GitHub: github.com/vincent040   Bug Report: 2437231462@qq.com
-//
-//////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////
 
 #include <stdio.h>
 #include <stdlib.h>
